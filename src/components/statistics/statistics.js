e48dd55b573d3b934b14dff11da46a11ee5e57df
Dmytro82 <dmutro8228@gmail.com>
2022-01-06 23:50:20 +0200

    test

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -4,7 +4,7 @@ import styles from './statistics.module.css';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={styles.statistics}>
-      <h2 className={styles.headline}>Exercise №2 statistics</h2>
+      <h2 className={styles.headline}>ghbkn.l.,</h2>
 
       {title && <h2 className={styles.title}>{title}</h2>}
 
