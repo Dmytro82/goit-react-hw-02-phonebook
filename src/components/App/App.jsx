d530ca88cd678c4240b0aca3f40f8d9bfbf382f8
Dmytro82 <dmutro8228@gmail.com>
2022-01-12 02:28:09 +0200

    test

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './App.styled';
+
+function App() {
+  return (
+    <Container>
+      <Title>phonebook</Title>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  background-color: hotpink;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  color: #ecaaaa;
+  padding-top: 20px;
+  text-transform: uppercase;
+`;
